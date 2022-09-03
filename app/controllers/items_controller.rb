74150fdeb99a4786f8ca9719f06adb4168c751f9
taigaggg <t.g.kodacc@gmail.com>
2022-09-03 15:33:31 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , only:[:new]
+  before_action :authenticate_user! , only:[:new,:edit]
   # ログイン画面へ誘導
  
   def index
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
