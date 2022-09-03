5e19a46d14514a7e56da107352ec2226dc4dbbdf
taigaggg <t.g.kodacc@gmail.com>
2022-09-03 17:52:11 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,8 +42,8 @@ class ItemsController < ApplicationController
   def destroy
     if current_user.id == @item.user_id
       @item.destroy
-      redirect_to action: :index
     end
+    redirect_to action: :index
   end
 
   private
